f83b58a15fe78d0cb7c1abc364d8c641cfee787b
Alexander <nefest71@gmail.com>
2023-11-02 17:00:48 +0300

    Created JobListModel

--- PartTimeJobs/Core/Flow/JobList/Scene/Model/JobListModel.swift
+++ PartTimeJobs/Core/Flow/JobList/Scene/Model/JobListModel.swift
@@ -7,6 +7,36 @@
 
 import Foundation
 
+struct Job: Decodable {
+    let id: String
+    let profession: String
+    let employer: String
+    let salary: Double
+    let date: Date
+    let logo: String?
+}
+
 struct JobListModel {
     
+    var jobList: [Job] { privateJobList }
+    
+    private var privateJobList: [Job] = [] {
+        didSet {
+            
+        }
+    }
+    
+    init() {}
+    
+    mutating func addJob(_ job: Job) {
+        privateJobList.append(job)
+    }
+    
+    mutating func removeJob(at index: Int) {
+        privateJobList.remove(at: index)
+    }
+    
+    mutating func updateJobs(_ jobs: [Job]) {
+        privateJobList = jobs
+    }
 }
