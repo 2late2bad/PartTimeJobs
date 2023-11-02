032bb41ce3a758427d0ef1100a7dc0efab633d9e
Alexander <nefest71@gmail.com>
2023-11-02 13:06:19 +0300

    Initial commit

---
+++ PartTimeJobs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PartTimeJobs
+//
+//  Created by Alexander V. on 02.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
