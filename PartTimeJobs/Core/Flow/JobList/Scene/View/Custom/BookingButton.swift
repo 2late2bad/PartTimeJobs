7902cc48aeff175c98e798d3b8387806c556962e
Alexander <nefest71@gmail.com>
2023-11-05 19:14:14 +0300

    chore: folder reorganization

--- PartTimeJobs/Core/Flow/JobList/Scene/View/Custom/BookingButton.swift
+++ PartTimeJobs/Core/Flow/JobList/Scene/View/Custom/BookingButton.swift
@@ -20,12 +20,13 @@ final class BookingButton: UIButton {
     }
     
     // MARK: - Methods
-    func configure(with title: String, isActive: Bool) {
+    func configure(with title: String?, isActive: Bool) {
         setTitleKeepingFont(title)
         isEnabled = isActive
     }
     
     private func setupSelf() {
+        startAnimatingPressActions()
         translatesAutoresizingMaskIntoConstraints = false
         isEnabled = false
         
@@ -33,11 +34,27 @@ final class BookingButton: UIButton {
         configuration.baseBackgroundColor = R.Colors.mainInteractive.ui
         configuration.baseForegroundColor = .label
         configuration.background.cornerRadius = 7
+        configuration.background.strokeWidth = 0.5
+        configuration.background.strokeColor = R.Colors.borderDefault.ui.withAlphaComponent(0.04)
         
         var container = AttributeContainer()
         container.font = .systemFont(ofSize: 15, weight: .semibold)
         configuration.attributedTitle = AttributedString("Выберите подработки", attributes: container)
         
         self.configuration = configuration
+    }
+    
+    override func updateConfiguration() {
+        var updatedConfiguration = configuration
+        switch state {
+        case .normal:
+            updatedConfiguration?.background.backgroundColor = R.Colors.mainInteractive.ui
+        case .highlighted:
+            updatedConfiguration?.background.backgroundColor = R.Colors.mainInteractive.ui.withAlphaComponent(0.5)
+        case .disabled:
+            updatedConfiguration?.background.backgroundColor = R.Colors.secondaryInteractive.ui
+        default: break
+        }
+        self.configuration = updatedConfiguration
     }
 }
